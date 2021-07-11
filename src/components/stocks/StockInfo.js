12f4a8703f6fc987a8138d9275baa0b69c80a48b
laojason <jasonlao1993@hotmail.com>
2021-07-11 01:14:20 -0700

    added express for backend, search button, and multiple stock display

--- src/components/stocks/StockInfo.js
+++ src/components/stocks/StockInfo.js
@@ -3,7 +3,11 @@ import React from 'react'
 const StockInfo = ({ stock }) => {
     return (
         <div>
-            {stock}
+            {stock.key}
+            <h1>Stock Ticker: {stock.symbol}</h1>
+            <h2>Price at close: ${stock.price}</h2>
+            <h3>Volume: {new Intl.NumberFormat().format(parseInt(stock.volume))}</h3>
+
         </div>
     )
 }
